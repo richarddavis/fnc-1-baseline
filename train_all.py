e9955fc8b6a1d9b61eefa22f4d69e6e815c69039
Chris Proctor <chris.proctor@gmail.com>
2017-03-19 17:01:23 -0700

    Updating split logic. New util methods, checking in test/other ids, updating scripts

--- train_all.py
+++ train_all.py
@@ -1,25 +1,26 @@
 from utils.dataset import DataSet
 from utils.generate_data import generate_data, collapse_stances
-from utils.generate_test_splits import generate_training_test_validation_split, read_ids
+from utils.generate_test_splits import generate_test_set, generate_train_val_sets, read_ids
 from configurations.ff_concat_two_losses import ff_concat_two_losses as conf
 from fnc_config import FNCConfig
 
 d = DataSet()
-generate_training_test_validation_split(d)
 base_dir = "splits"
-training_ids = read_ids("training_ids.txt", base_dir)
-X_train_headline, X__train_article, y_train = generate_data(training_ids, d)
-test_ids = read_ids("test_ids.txt", base_dir)
-X_test_headline, X_test_article, y_test = generate_data(test_ids, d)
+# generate_test_set(d, test=0.15) # This was already run once; now we will keep using the same IDs.
+generate_train_val_sets(d, val=0.2)
+
+train_ids = read_ids("train_ids.txt", base_dir)
+X_train_headline, X_train_article, y_train = generate_data(train_ids, d)
+val_ids = read_ids("val_ids.txt", base_dir)
+X_val_headline, X_val_article, y_val = generate_data(val_ids, d)
 
 for conf in FNCConfig.get_untrained():
     m = conf.get_model()
     model, history = m.train(
-        [X_train_headline, X__train_article], 
+        [X_train_headline, X_train_article], 
         y_train,
-        [X_test_headline, X_test_article],
-        y_test
+        [X_val_headline, X_val_article],
+        y_val
     )
     conf.save(model, history)
-
 
--- utils/generate_test_splits.py
+++ utils/generate_test_splits.py
@@ -21,34 +21,40 @@ def generate_hold_out_split (dataset, training = 0.8, base_dir="splits"):
     with open(base_dir+ "/"+ "hold_out_ids.txt", "w+") as f:
         f.write("\n".join([str(id) for id in hold_out_ids]))
 
-def generate_training_test_validation_split(dataset, test=0.2, validation=0.15, base_dir="splits"):
+def generate_test_set(dataset, test=0.15, base_dir="splits"):
+    "Generates a new test set to hold out permanently"
     r = random.Random()
-    r.seed(1489215) # do not change this. 
-
     article_ids = list(dataset.articles.keys())  # get a list of article ids
     r.shuffle(article_ids)  # and shuffle that list
-
-    training_ids = article_ids[:int((1 - test - validation) * len(article_ids))]
-    test_ids = article_ids[int((1 - test - validation) * len(article_ids)) : int((1 - validation) * len(article_ids))]
-    validation_ids = article_ids[int((1 - validation) * len(article_ids)):]
-
-    print("Generated split: training: {}, test: {}, validation: {}".format(len(training_ids), len(test_ids), len(validation_ids)))
-
-    # write the split body ids out to files for future use
-    with open(base_dir+ "/"+ "training_ids.txt", "w+") as f:
-        f.write("\n".join([str(id) for id in training_ids]))
+    test_ids = article_ids[:int(test * len(article_ids))]
+    other_ids = article_ids[int(test * len(article_ids)):]
+    print("Generated a test set ({}) and other ({})".format(len(test_ids), len(other_ids)))
     with open(base_dir+ "/"+ "test_ids.txt", "w+") as f:
         f.write("\n".join([str(id) for id in test_ids]))
-    with open(base_dir+ "/"+ "validation_ids.txt", "w+") as f:
-        f.write("\n".join([str(id) for id in validation_ids]))
+    with open(base_dir+ "/"+ "other_ids.txt", "w+") as f:
+        f.write("\n".join([str(id) for id in other_ids]))
 
+def generate_train_val_sets(dataset, val=0.15, base_dir="splits"):
+    try: 
+        ids = read_ids("other_ids.txt", base_dir)
+    except FileNotFoundError:
+        raise FileNotFoundError("IDs file {} not found. Run `generate_test_set` first.")
+    r = random.Random()
+    r.shuffle(ids)
+    val_ids = ids[:int(val * len(ids))]
+    train_ids = ids[int(val * len(ids)):]
+    print("Generated a validation set ({}) and a train set ({})".format(len(val_ids), len(train_ids)))
+    with open(base_dir+ "/"+ "val_ids.txt", "w+") as f:
+        f.write("\n".join([str(id) for id in val_ids]))
+    with open(base_dir+ "/"+ "train_ids.txt", "w+") as f:
+        f.write("\n".join([str(id) for id in train_ids]))
+    
 def read_ids(file,base):
     ids = []
     with open(base+"/"+file,"r") as f:
         for line in f:
            ids.append(int(line))
         return ids
-
 
 def kfold_split(dataset, training = 0.8, n_folds = 10, base_dir="splits"):
     if not (os.path.exists(base_dir+ "/"+ "training_ids.txt")
@@ -63,7 +69,6 @@ def kfold_split(dataset, training = 0.8, n_folds = 10, base_dir="splits"):
         folds.append(training_ids[int(k*len(training_ids)/n_folds):int((k+1)*len(training_ids)/n_folds)])
 
     return folds,hold_out_ids
-
 
 def get_stances_for_folds(dataset,folds,hold_out):
     stances_folds = defaultdict(list)
